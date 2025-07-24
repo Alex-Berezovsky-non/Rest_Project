d15ce9d6e788e16361f449f66ae33c52644a4e42
Alex Berezovsky <oxonkoalexey@gmail.com>
2025-07-24 20:19:03 +0300

    RU: FIX: Отображение отзывов на главной странице ENG: FIX: Display reviews on homepage

--- core/views.py
+++ core/views.py
@@ -1,19 +1,13 @@
-from typing import Any
 from django.views.generic import TemplateView
-from reviews.views import ReviewListView
 
-class HomeView(ReviewListView):
+class HomeView(TemplateView):
     template_name = 'core/home.html'
-    
-    def get_context_data(self, **kwargs: dict[str, Any]) -> dict[str, Any]:
-        context = super().get_context_data(**kwargs)
-        context.update({
-            'page_title': 'Главная',
-            'welcome_message': 'Добро пожаловать в Al Dente!',
-            'address': 'ул. Итальянская, 42',
-            'phone': '+7 (123) 456-78-90'
-        })
-        return context
+    extra_context = {
+        'page_title': 'Главная',
+        'welcome_message': 'Добро пожаловать в Al Dente!',
+        'address': 'ул. Итальянская, 42',
+        'phone': '+7 (123) 456-78-90'
+    }
 
 class ContactsView(TemplateView):
     template_name = 'core/contacts.html'
