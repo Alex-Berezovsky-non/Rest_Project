d9669db144b3a154d1344d885e11d92e8c6bb2ff
Alex Berezovsky <oxonkoalexey@gmail.com>
2025-07-24 20:14:41 +0300

    RU: config: Добавлены стили отзывов
    - Созданы CSS стили для системы отзывов
    ENG:
    config: Add reviews styles
    - CSS styles for the feedback system have been created

--- core/views.py
+++ core/views.py
@@ -1,13 +1,19 @@
+from typing import Any
 from django.views.generic import TemplateView
+from reviews.views import ReviewListView
 
-class HomeView(TemplateView):
+class HomeView(ReviewListView):
     template_name = 'core/home.html'
-    extra_context = {
-        'page_title': 'Главная',
-        'welcome_message': 'Добро пожаловать в Al Dente!',
-        'address': 'ул. Итальянская, 42',
-        'phone': '+7 (123) 456-78-90'
-    }
+    
+    def get_context_data(self, **kwargs: dict[str, Any]) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context.update({
+            'page_title': 'Главная',
+            'welcome_message': 'Добро пожаловать в Al Dente!',
+            'address': 'ул. Итальянская, 42',
+            'phone': '+7 (123) 456-78-90'
+        })
+        return context
 
 class ContactsView(TemplateView):
     template_name = 'core/contacts.html'
