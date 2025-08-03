cefd254ed6ded970b86fe7a71348100fffe310f6
Alex Berezovsky <oxonkoalexey@gmail.com>
2025-08-04 00:00:47 +0300

    ENG: feat(ui): обновлена навигация ресторана - Добавлены все основные разделы (Меню, Бронирование, Галерея, Команда, Отзывы, События, Контакты) - Реализован выпадающий список для Галереи - Добавлен поиск по меню - Улучшена адаптивность меню ENG: Update restaurant navigation menu

--- events/urls.py
+++ events/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import UpcomingEventsView, EventDetailView
 
+app_name = 'events' 
 urlpatterns = [
     path('', UpcomingEventsView.as_view(), name='upcoming_events'),
     path('<int:pk>/', EventDetailView.as_view(), name='event_detail'),
